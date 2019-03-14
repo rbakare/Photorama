521027d6a76337bb4f5be77aba0c289f149f7a26
rbakare <hadeybaks@gmail.com>
2019-03-14 19:04:27 -0400

    Initial Commit

---
+++ Photorama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Raheem Bakare on 2019-03-14.
+//  Copyright © 2019 centennial college. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
