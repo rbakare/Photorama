dc8652cba574571ab466ce723b4667f17e588a06
rbakare <hadeybaks@gmail.com>
2019-03-14 21:27:44 -0400

    photo rama

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Raheem Bakare on 2019-03-14.
+//  Copyright © 2019 centennial college. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchInterestingPhotos() {
+            (photosResult) -> Void in
+            
+            switch photosResult {
+            case let .success(photos):
+                print("Successfully found \(photos.count) photos.")
+                if let firstPhoto = photos.first {
+                    self.updateImageView(for: firstPhoto)
+                }
+            case let .failure(error):
+                print("Error fetching interesting photos: \(error)")
+            }
+        }
+    }
+    
+    @IBOutlet var imageView: UIImageView!
+    var store: PhotoStore!
+    
+    func updateImageView(for photo: Photo) {
+        store.fetchImage(for: photo) {
+            (imageResult) -> Void in
+            
+            switch imageResult {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error downloading image: \(error)")
+            }
+        }
+    }
+}
